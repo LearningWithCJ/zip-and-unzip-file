3ac568e16dbd6aea13ccf14607340484a3e82710
CJ <110461799+LearningWithCJ@users.noreply.github.com>
2024-08-22 17:42:57 +0330

    zip.py

---
+++ zip.py
@@ -0,0 +1,24 @@
+#
+#  _                                _                __          __ _  _    _        _____      _ 
+# | |                              (_)               \ \        / /(_)| |  | |      / ____|    | |
+# | |      ___   __ _  _ __  _ __   _  _ __    __ _   \ \  /\  / /  _ | |_ | |__   | |         | |
+# | |     / _ \ / _` || '__|| '_ \ | || '_ \  / _` |   \ \/  \/ /  | || __|| '_ \  | |     _   | |
+# | |____|  __/| (_| || |   | | | || || | | || (_| |    \  /\  /   | || |_ | | | | | |____| |__| |
+# |______|\___| \__,_||_|   |_| |_||_||_| |_| \__, |     \/  \/    |_| \__||_| |_|  \_____|\____/ 
+#                                              __/ |                                              
+#                                             |___/                         -  By CJ
+#
+# YouTube : www.youtube.com/@LearningWithCJ
+# GitHub  : www.github.com/LearningWithCJ
+# Telegram: t.me/LearningWithCJ
+#
+
+import zipfile, os
+
+FILE_PATH = "" # enter path you wanna zip
+ZIP_PATH = "" # enter path you wanna save zip file
+
+with zipfile.ZipFile(ZIP_PATH, 'w') as zipFile:
+    for root, dirs, files in os.walk(FILE_PATH):
+        for file in files:
+            zipFile.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(FILE_PATH, '..')))
