c2a8e51057a2e8ba12faa17036c504ad50a6f955
CJ <110461799+LearningWithCJ@users.noreply.github.com>
2024-08-22 17:43:44 +0330

    unzip.py

---
+++ unzip.py
@@ -0,0 +1,22 @@
+#
+#  _                                _                __          __ _  _    _        _____      _ 
+# | |                              (_)               \ \        / /(_)| |  | |      / ____|    | |
+# | |      ___   __ _  _ __  _ __   _  _ __    __ _   \ \  /\  / /  _ | |_ | |__   | |         | |
+# | |     / _ \ / _` || '__|| '_ \ | || '_ \  / _` |   \ \/  \/ /  | || __|| '_ \  | |     _   | |
+# | |____|  __/| (_| || |   | | | || || | | || (_| |    \  /\  /   | || |_ | | | | | |____| |__| |
+# |______|\___| \__,_||_|   |_| |_||_||_| |_| \__, |     \/  \/    |_| \__||_| |_|  \_____|\____/ 
+#                                              __/ |                                              
+#                                             |___/                         -  By CJ
+#
+# YouTube : www.youtube.com/@LearningWithCJ
+# GitHub  : www.github.com/LearningWithCJ
+# Telegram: t.me/LearningWithCJ
+#
+
+import zipfile
+
+ZIP_PATH = "" # enter zip file path
+UNZIP_PATH = "" # enter path you wanna save unzip file
+
+with zipfile.ZipFile(ZIP_PATH, "r") as zipFile:
+    zipFile.extractall(path=UNZIP_PATH)
